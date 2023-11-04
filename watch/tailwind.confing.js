5ea768c530e6e2ec6a3db7578d81cac5458e343c
PRASHANTH CHOWDARY <ch.prashanthchowdary007@gmail.com>
2023-11-04 19:06:42 +0530

    20percent complete

---
+++ watch/tailwind.confing.js
@@ -0,0 +1,14 @@
+module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          customRed: '#FF0000', // Replace with your desired color code
+        },
+        minHeight: {
+          '1/2': '50vh',
+        },
+      },
+    },
+    // Other Tailwind CSS configurations
+    plugins: [],
+  };
